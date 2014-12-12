0abbc5265fae8da8117c68785ad2462fb284fce5
Szikszai Gusztáv <guszti5@hotmail.com>
2014-12-12 13:47:26 +0100

    CI.

--- source/javascripts/main.rb
+++ source/javascripts/main.rb
@@ -1,12 +1,23 @@
 require 'fron'
 
+# Kernel
 module Kernel
+  # Yields every ms time
+  #
+  # @param ms [Integer] The interval
   def interval(ms = 0)
     `setInterval(function(){#{yield}},#{ms})`
   end
 end
 
+# Numeric
 class Numeric
+  # Clamps the number between a minimum and maximum value.
+  #
+  # @param min [Numeric] The minimum value
+  # @param max [Numeric] The maximum value
+  #
+  # @return [Numeric] The clamped value
   def clamp(min, max)
     [[self, max].min, min].max
   end
@@ -14,7 +25,8 @@ end
 
 # Neko
 class Neko < Fron::Component
-  tag 'neko[name=calico]'
+  NAMES = %w(blue calico gray holiday valentine)
+
   STATES = {
     hungry: {
       method: :eat,
@@ -45,12 +57,13 @@ class Neko < Fron::Component
       success_score: 0
     },
     idle: {
-      method: :scratch,
+      method: :scratch
     }
   }
 
   attr_reader :health
 
+  # Define states for methods
   STATES.each do |key, state|
     define_method state[:method] do
       return unless @state == key
@@ -61,27 +74,29 @@ class Neko < Fron::Component
   # Initializes the neko
   def initialize
     super
+    self[:name] = NAMES.sample
     @health = 100
     idle
   end
 
+  # Runs on every tick
   def tick
     return if dead?
+    state, _ = next_state
 
-    if idle?
-      state, _ = next_state
-      if state
-        @start_time = Time.now
-        set_state state
-        puts "Next state: #{state}"
-      end
+    if idle? && state
+      @start_time = Time.now
+      set_state state
+      puts "Next state: #{state}"
     else
       diff = Time.now - @start_time
       end_state if diff > STATES[@state][:duration]
     end
-
   end
 
+  # Sets the state to the given state
+  #
+  # @param state [String] The state
   def set_state(state)
     @state = state
     self[:action] = state
@@ -89,20 +104,18 @@ class Neko < Fron::Component
     trigger 'change'
   end
 
+  # Successfull resolve
   def resolve_state
     @health += STATES[@state][:success_score]
-    puts 'Success...'
+    puts 'Successfull resolve!'
     idle
   end
 
+  # Fail to resolve
   def end_state
     @health -= STATES[@state][:fail_score]
-    puts "Failed: health is now #{@health}"
-    if @health > 0
-      idle
-    else
-      die
-    end
+    @health > 0 ? idle : die
+    puts "Failed to resolve! Health is now #{@health}!"
   end
 
   # Gets the next state from random number
@@ -116,49 +129,62 @@ class Neko < Fron::Component
     end
   end
 
+  # Kills the neko
   def die
     set_state :dead
-    puts 'Your cat is dead!'
+    puts 'Your neko is dead!'
   end
 
+  # Sets the neko to idle
   def idle
     set_state :idle
     @start_time = nil
   end
 
+  # Returns if the neko is dead or not.
+  #
+  # @return [Boolean] The value
   def dead?
     @state == :dead
   end
 
+  # Returns if the neko is idle or not.
+  #
+  # @return [Boolean] The value
   def idle?
     @state == :idle
   end
 end
 
+# Main
 class Main < Fron::Component
   component :neko, Neko
   component :health, 'div'
 
-  Neko::STATES.each do |key, state|
+  Neko::STATES.each do |_, state|
     component :key, "button[action=#{state[:method]}] #{state[:method]}"
   end
 
   on :click, :button, :action
   on :change, :render
 
+  # Sends an action to neko
+  #
+  # @param event [Event] The event
   def action(event)
     @neko.send event.target[:action]
   end
 
+  # Renders the component
   def render
     @health.text = @neko.health
   end
 
+  # Initializes the component
   def initialize
     super
-    interval 1000 do
-      @neko.tick
-    end
+    interval(1000) { @neko.tick }
   end
 end
+
 DOM::Document.body << Main.new
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,7 +1,1 @@
 require 'spec_helper'
-
-describe 'Test' do
-  it 'should add a message to the body' do
-    DOM::Document.body.find('div').text.should eq 'Hello from Fron!'
-  end
-end
